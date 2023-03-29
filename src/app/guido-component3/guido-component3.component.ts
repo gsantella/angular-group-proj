a6fc39f73da0bc4b5672f3068065d2d9d1d0164d
Guido Santella <gsantella@southhills.edu>
2023-03-29 09:52:06 -0400

    starter for weapon component

--- src/app/guido-component3/guido-component3.component.ts
+++ src/app/guido-component3/guido-component3.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./guido-component3.component.css']
 })
 export class GuidoComponent3Component {
+  currentRounds = 15
+
+  fire() {
+    this.currentRounds = (this.currentRounds > 0) ? this.currentRounds - 1 : 0
+  }
+
+  reload() {
+    this.currentRounds = 10
+  }
 
 }
