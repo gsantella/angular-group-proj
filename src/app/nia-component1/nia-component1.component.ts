d297bf39ccc4987e6a565d5a1e26e494e68143ac
Nia <nmcalkich71@southhills.edu>
2023-03-29 10:05:31 -0400

    Committing so I don't lose changes

--- src/app/nia-component1/nia-component1.component.ts
+++ src/app/nia-component1/nia-component1.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-nia-component1',
@@ -6,5 +6,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./nia-component1.component.css']
 })
 export class NiaComponent1Component {
+
+  celTemp = 'celTemp'
+  sub1 = 'sub1'
+  res1 = 'res1' 
+  farTemp = 'farTemp'
+  sub2 = 'sub2'
+  res2 = 'res2'
+
+  alert() {
+    
+  }
+
+  cToF() {
+    
+  }
 
 }
