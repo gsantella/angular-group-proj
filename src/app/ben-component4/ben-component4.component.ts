486db755d7ece7e7c2ef0009476c7baab0d3b644
SpoodsTheDood <75pulsar75@gmail.com>
2023-04-05 10:00:16 -0400

    started working on the API, not much left

--- src/app/ben-component4/ben-component4.component.ts
+++ src/app/ben-component4/ben-component4.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-ben-component4',
@@ -6,5 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./ben-component4.component.css']
 })
 export class BenComponent4Component {
+  pictureOfAShibe:any
+  constructor(private httpClient:HttpClient){}
+shibePics(){
+this.pictureOfAShibe = this.httpClient.get("http://shibe.online/api/shibes?count=1&urls=true&httpsUrls=true")
 
+}
 }
